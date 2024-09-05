10222574e467c808bab5a0b2d1a2d6b1e55c4011
analiviag <126291217+analiviag@users.noreply.github.com>
2024-09-05 13:21:32 -0300

    created react application and transferred weather app into it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [city, setCity] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState("");
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "203fa770242fcd2b9555d832a88ea567";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <div>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type a city"
+          onChange={updateCity}
+        ></input>
+        <input type="submit" value="Search" />
+      </form>
     </div>
   );
-}
 
-export default App;
+  if (loaded) {
+    return (
+      <div>
+        {form}
+
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Wind: {weather.wind}</li>
+          <li>Humidity: {weather.humidity}</li>
+          <li>
+            <img src={weather.icon} alt="weather icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
